9fe7bfcbe4a7ac00ddaaef7504122ec2599a155c
thepigcat <benpospo@gmail.com>
2024-07-24 15:14:59 +0200

    initial commit

---
+++ Dino.ino
@@ -0,0 +1,434 @@
+// Ausführen mit ./upload.bat
+
+// TODO: Änimäschions
+
+#include "aelements.h"
+#include <EEPROM.h>
+#include <LiquidCrystal_I2C.h>
+#include <stdint.h>
+
+#define I2C_ADDR 0x27
+// Die Display Adresse
+#define LCD_COLUMNS 20
+// Anzahl der Spalten
+#define LCD_LINES 4
+// Anzahl der Zeilen
+LiquidCrystal_I2C lcd(I2C_ADDR, LCD_COLUMNS, LCD_LINES);
+
+struct Cactus {
+  uint8_t x;
+  bool tall;
+};
+
+uint16_t score = 0;
+uint16_t highscore = 0;
+
+uint8_t gameDelay = 200;
+
+uint8_t playerPos = 2;
+bool jumping = false;
+
+uint8_t velocity = 0;
+
+uint8_t selectedPlayerSkin = 0;
+
+bool right = false;
+bool down = false;
+bool left = false;
+bool up = false;
+bool push = false;
+
+// time after character
+// is selected until game starts (miliseconds)
+#define TIME_BEFORE_GAME_START 2000
+
+#define STARTSCREEN 0
+#define CHAR_SEL_SCREEN 1
+#define GAME_SCREEN 2
+#define END_SCREEN 3
+
+#define HIGH_SCORE_ADDR 0
+
+#define DISTANCE_BEFORE_CACTUS 3
+
+// 0 = character selector
+// 1 = game
+// 2 = end screen
+uint8_t curScreen = 0;
+
+Cactus cacti[LCD_COLUMNS];
+uint8_t cacti_amount = 0;
+
+uint8_t cactus_distance = 0;
+
+#define VERT_PIN A0
+#define HORZ_PIN A1
+#define SEL_PIN 2
+#define LED_PIN 9
+#define BUT_PIN 3
+
+void setup() {
+  // Start des Displays
+  lcd.init();
+  lcd.backlight();
+
+  // Joystick starten
+  pinMode(VERT_PIN, INPUT);
+  pinMode(HORZ_PIN, INPUT);
+  pinMode(SEL_PIN, INPUT_PULLUP);
+
+  // LED
+  pinMode(LED_PIN, OUTPUT);
+
+  // Button
+  pinMode(BUT_PIN, INPUT_PULLUP);
+  highscore = EEPROM.read(HIGH_SCORE_ADDR);
+}
+
+void loop() {
+  if (curScreen == STARTSCREEN) {
+    createStartElements();
+    handleInput();
+    startscreen();
+  } else if (curScreen == CHAR_SEL_SCREEN) {
+    createGeneralElements();
+    handleInput();
+    charSelScreen();
+  } else if (curScreen == GAME_SCREEN) {
+    if (score % 20 == 0) {
+      gameDelay -= 2;
+    }
+    createGeneralElements();
+    // Handle stuff
+    handleInput();
+    handleJump();
+    handleCollision();
+    // General tasks
+    score++;
+    cactus_distance++;
+    listenForJoystickInput();
+    // Draw tasks
+    drawGround();
+    drawCacti();
+    drawPlayer();
+    drawScore();
+    lcd.setCursor(14, 0);
+    lcd.print("H:");
+    lcd.print(highscore);
+    // Delay so the game is playable
+    delay(gameDelay);
+  } else if (curScreen == END_SCREEN) {
+    createEndElements();
+    handleInput();
+    endscreen();
+  }
+  resetHighscore();
+}
+
+void createGeneralElements() {
+  // Erstellen der Custom Chars
+  lcd.createChar(0, geist);
+  lcd.createChar(1, mate);
+  lcd.createChar(2, anselm);
+  lcd.createChar(3, bigman);
+  lcd.createChar(4, arrow);
+
+  lcd.createChar(5, boden);
+  lcd.createChar(6, grossKak);
+  lcd.createChar(7, kleinKak);
+}
+
+void createStartElements() {
+  // Erstellen der Custom Startscreen Chars
+  lcd.createChar(0, D_Top);
+  lcd.createChar(1, D_Bot);
+  lcd.createChar(2, dot_I_Top);
+  lcd.createChar(3, dot_I_Bot);
+  lcd.createChar(4, N_Top);
+  lcd.createChar(5, N_Bot);
+  lcd.createChar(6, O_Top);
+  lcd.createChar(7, O_Bot);
+}
+
+void createEndElements() { lcd.createChar(0, skull); }
+
+void listenForJoystickInput() {
+  if (up && !jumping && velocity <= 0) {
+    startJump();
+  }
+}
+
+void drawGround() {
+  for (uint8_t x = 0; x < LCD_COLUMNS; x++) {
+    lcd.setCursor(x, LCD_LINES - 1);
+    lcd.write(byte(5));
+  }
+}
+
+void drawCacti() {
+  // Generate new cacti based on a random chance
+  uint8_t rand_int = random(0, 18);
+
+  if (cactus_distance >= DISTANCE_BEFORE_CACTUS && rand_int == 1) {
+    cacti[cacti_amount].x = LCD_COLUMNS - 1;
+    cacti[cacti_amount].tall = true;
+    cacti_amount++;
+    cactus_distance = 0;
+  } else if (cactus_distance >= DISTANCE_BEFORE_CACTUS &&
+             (rand_int == 2 || rand_int == 3)) {
+    cacti[cacti_amount].x = LCD_COLUMNS - 1;
+    cacti[cacti_amount].tall = false;
+    cacti_amount++;
+    cactus_distance = 0;
+  }
+
+  // Draw cacti
+  uint8_t removed_amount = 0;
+
+  clearLine(2);
+  for (uint8_t x = 0; x < cacti_amount; x++) {
+    lcd.setCursor(cacti[x].x, 2);
+    if (cacti[x].tall) {
+      lcd.write(byte(6));
+    } else {
+      lcd.write(byte(7));
+    }
+    if (cacti[x].x <= 0) {
+      removed_amount++;
+    }
+    cacti[x].x--;
+  }
+
+  cleanup_array(cacti, cacti_amount, removed_amount);
+  cacti_amount -= removed_amount;
+}
+
+void drawPlayer() {
+  lcd.setCursor(3, playerPos);
+  lcd.write(byte(selectedPlayerSkin));
+}
+
+void drawScore() {
+  lcd.setCursor(0, 0);
+  lcd.print(score);
+}
+
+void handleInput() {
+  int horz = analogRead(HORZ_PIN);
+  int vert = analogRead(VERT_PIN);
+  bool pressed = digitalRead(SEL_PIN) == LOW;
+  if (vert <= 200) {
+    down = false;
+    up = true;
+  } else if (vert >= 1000) {
+    down = true;
+    up = false;
+  } else {
+    down = false;
+    up = false;
+  }
+
+  if (horz <= 100) {
+    right = false;
+    left = true;
+  } else if (horz >= 1000) {
+    right = true;
+    left = false;
+  } else {
+    right = false;
+    left = false;
+  }
+
+  if (pressed) {
+    push = true;
+  } else {
+    push = false;
+  }
+}
+
+void startJump() {
+  jumping = true;
+  velocity = 3;
+  playerPos = 1;
+}
+
+void handleJump() {
+  if (velocity != 0) {
+    velocity -= 1;
+  }
+
+  if (velocity <= 1) {
+    playerPos = 2;
+    clearLine(1);
+    jumping = false;
+  }
+}
+
+void press() {}
+
+void cleanup_array(Cactus *arr, uint8_t arr_len, uint8_t offset) {
+  for (uint8_t i = offset; i < arr_len; i++) {
+    arr[i - offset] = arr[i];
+  }
+}
+
+void handleCollision() {
+  if (cacti_amount != 0 && cacti[0].x == 3 && !jumping) {
+    curScreen = END_SCREEN;
+  }
+}
+void startscreen() {
+  lcd.setCursor(7, 1);
+  // Printet Title
+  for (uint8_t x = 0; x < 4; x++) {
+    lcd.setCursor(x + 8, 0);
+    lcd.write(x * 2);
+  }
+  for (uint8_t x = 0; x < 4; x++) {
+    lcd.setCursor(x + 8, 1);
+    lcd.write(x * 2 + 1);
+  }
+  lcd.setCursor(3, 3);
+  lcd.print("PRESS TO START");
+  // Printet Randlinien
+  for (uint8_t x = 0; x < 4; x++) {
+    lcd.setCursor(0, x);
+    lcd.print("|");
+  }
+  for (uint8_t x = 0; x < 4; x++) {
+    lcd.setCursor(19, x);
+    lcd.print("|");
+  }
+  delay(255);
+  clearLineStart(2);
+  if (push || left || right || up || down) {
+    highscore = EEPROM.read(HIGH_SCORE_ADDR);
+    curScreen = CHAR_SEL_SCREEN;
+    lcd.clear();
+  }
+}
+
+void endscreen() {
+  lcd.clear();
+  lcd.setCursor(5, 1);
+  lcd.print("Game Over");
+  lcd.setCursor(8, 2);
+  lcd.print(score);
+  lcd.setCursor(6, 3);
+  lcd.print("Restart");
+  lcd.setCursor(9, 0);
+  lcd.write(byte(0));
+  if (score > highscore) {
+    highscore = score;
+  }
+  if (push) {
+    curScreen = STARTSCREEN;
+    clearLine(1);
+    EEPROM.write(HIGH_SCORE_ADDR, highscore);
+    resetAll();
+    push = false;
+  }
+  delay(500);
+}
+
+void clearLine(uint8_t line) {
+  lcd.setCursor(0, line);
+  lcd.print("                     ");
+}
+void clearLineStart(uint8_t line) {
+  lcd.setCursor(3, line);
+  lcd.print("              ");
+}
+void resetAll() {
+  lcd.clear();
+  cacti_amount = 0;
+  score = 0;
+  playerPos = 2;
+}
+
+// CHARACTERSELECTION
+typedef char *string_t;
+
+string_t names[] = {"Geist", "Mate", "Anselm", "Big Man"};
+
+uint8_t selCharacter = 3;
+
+void charSelScreen() {
+  // Print CharName
+  lcd.setCursor(7, 0);
+  lcd.print(names[selCharacter]);
+
+  // Printet die auswählbaren Characters
+  lcd.setCursor(7, 3);
+  lcd.write(byte(0));
+  lcd.setCursor(9, 3);
+  lcd.write(byte(1));
+  lcd.setCursor(11, 3);
+  lcd.write(byte(2));
+  lcd.setCursor(13, 3);
+  lcd.write(byte(3));
+
+  // Printet Randlinien
+  for (uint8_t x = 0; x < 4; x++) {
+    lcd.setCursor(0, x);
+    lcd.print("|");
+  }
+  for (uint8_t x = 0; x < 4; x++) {
+    lcd.setCursor(19, x);
+    lcd.print("|");
+  }
+
+  // Verschiebt Arrow
+  if (left) {
+    selCharacter--;
+    if (selCharacter > 3) {
+      selCharacter = 0;
+    }
+    lcd.setCursor(selCharacter * 2 + 7, 2);
+    lcd.write(byte(4));
+    clearLineStart(0);
+    clearLineStart(2);
+    clearLineStart(3);
+  } else if (right) {
+    selCharacter++;
+    if (selCharacter > 3) {
+      selCharacter = 3;
+    }
+    lcd.setCursor(selCharacter * 2 + 7, 2);
+    // Draw arrow
+    lcd.write(byte(4));
+    clearLineStart(0);
+    clearLineStart(2);
+    clearLineStart(3);
+  } else {
+    lcd.setCursor(selCharacter * 2 + 7, 2);
+    // Draw arrow
+    lcd.write(byte(4));
+  }
+
+  // Select
+  if (push) {
+    lcd.clear();
+    lcd.setCursor(6, 1);
+    lcd.print("Selected");
+    lcd.setCursor(10, 2);
+    lcd.write(byte(selCharacter));
+    selectedPlayerSkin = selCharacter;
+    lcd.setCursor(7, 0);
+    lcd.print(names[selCharacter]);
+    delay(TIME_BEFORE_GAME_START);
+    curScreen = GAME_SCREEN;
+    lcd.clear();
+  }
+
+  delay(100);
+}
+
+void resetHighscore() {
+  bool but = digitalRead(BUT_PIN);
+
+  if (but == LOW) {
+    highscore = 0;
+    EEPROM.write(HIGH_SCORE_ADDR, highscore);
+  }
+}
---
+++ aelements.h
@@ -0,0 +1,57 @@
+typedef uint8_t BitTexture[];
+
+// Alle animationen & andere bilder
+// Datei braucht 'a', damit variablen benutzbar sind
+BitTexture grossKak = {B00100, B10100, B10100, B01101,
+                       B00101, B00110, B00100, B00100};
+// Kleiner Kaktus
+BitTexture kleinKak = {
+    B00000, B00000, B00000, B00100, B00101, B10110, B01100, B00100,
+};
+// Boden
+BitTexture boden = {
+    B11111, B10110, B00101, B01000, B00010, B10000, B00000, B00000,
+};
+// Select Pfeil
+BitTexture arrow = {B00100, B00100, B00100, B00100,
+                    B11111, B01110, B00100, B00000};
+// Geist
+BitTexture geist = {B01110, B11111, B10101, B11111,
+                    B11111, B10101, B10101, B00000};
+// 1.Figur(die ohne Gesicht)
+BitTexture mate = {B01110, B10001, B10001, B10001,
+                   B01110, B11011, B01110, B11011};
+// 3.Figur
+BitTexture anselm = {B11111, B10101, B11111, B01110,
+                     B11111, B11111, B01010, B01010};
+
+// 4.Figur
+BitTexture bigman = {B11111, B10101, B11111, B11111,
+                     B11111, B01010, B01010, B11011};
+// Totenschaedel
+BitTexture skull = {B00000, B01110, B11111, B10101,
+                    B11011, B01110, B01110, B00000};
+// STARTSCREEN
+// D Top (SC)
+BitTexture D_Top = {B11100, B11110, B11011, B11001,
+                    B11001, B11001, B11001, B11001};
+// D Bottom (SC)
+BitTexture D_Bot = {B11001, B11001, B11001, B11001,
+                    B11001, B11011, B11110, B11100};
+//.I (SC)
+BitTexture dot_I_Top = {B00011, B00011, B00011, B00011,
+                        B00011, B00011, B00011, B00011};
+BitTexture dot_I_Bot = {B00011, B00011, B00011, B00011,
+                        B00011, B00011, B11011, B11011};
+// N (SC)
+BitTexture N_Top = {B10001, B10001, B110001, B11001,
+                    B11001, B11001, B10101,  B10101};
+BitTexture N_Bot = {B10101, B10101, B10011, B10011,
+                    B10011, B10011, B10001, B10001};
+// O (SC)
+BitTexture O_Top = {
+    B01110, B11111, B11011, B11011, B11011, B11011, B11011, B11011,
+};
+BitTexture O_Bot = {
+    B11011, B11011, B11011, B11011, B11011, B11011, B11111, B01110,
+};
